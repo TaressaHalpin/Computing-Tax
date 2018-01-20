2a502dda581222e6a6e46c56106a7398d9e920c8
TaressaHalpin <thalpin2583@kvcc.edu>
2018-01-20 11:01:23 -0500

    COMPUTING TAX PROGRAM

---
+++ README.md
@@ -0,0 +1,102 @@
+﻿
+Computing Tax Program: 
+
+The United States federal personal income tax is calculated based on filing status and taxable income. 
+
+There are four filing statuses: single filers, married filing jointly, married filing separately, and head of household. 
+
+The tax rates vary every year. The table below shows the rates for 2009. 
+
+A Single Person with a taxable income of $10,000, the first $8,350 is taxed at 10% and the other $1,650 is taxed at 15%. So, your tax is $1,082.5.
+
+<table>
+  <tr>
+    <th>Marginal Tax Rate</th>
+    <th>Single</th>
+    <th>Married Filing Jointly or Qualified Widow(er)</th>
+    <th>Married Filing Separately</th>
+    <th>Head of Household</th>
+  </tr>
+  <tr>
+    <td>10%</td>
+    <td>$0 – $8,350</td>
+    <td>$0 – $16,700</td>
+    <td>$0 – $8,350</td>
+    <td>$0 – $11,950</td>
+  </tr>
+  <tr>
+    <td>15%</td>
+    <td>$8,351– $33,950</td>
+    <td>$16,701 – $67,900</td>
+    <td>$8,351 – $33,950</td>
+    <td>$11,951 – $45,500</td>
+  </tr>
+  <tr>
+    <td>25%</td>
+    <td>$33,951 – $82,250</td>
+    <td>$67,901 – $137,050</td>
+    <td>$33,951 – $68,525</td>
+    <td>$45,501 – $117,450</td>
+  </tr>
+  <tr>
+    <td>28%</td>
+    <td>$82,251 – $171,550</td>
+    <td>$137,051 – $208,850</td>
+    <td>$68,525 – $104,425</td>
+    <td>$117,451 – $190,200</td>
+  </tr>
+  <tr>
+    <td>33%</td>
+    <td>$171,551 – $372,950</td>
+    <td>$208,851 – $372,950</td>
+    <td>$104,426 – $186,475</td>
+    <td>$190,201 - $372,950</td>
+  </tr>
+  <tr>
+    <td>35%</td>
+    <td>$372,951+</td>
+    <td>$372,951+</td>
+    <td>$186,476+</td>
+    <td>$372,951+</td>
+  </tr>
+</table>
+
+Program computes personal income tax. 
+
+The Program should prompt the user to enter the filing status and taxable income and compute the tax. 
+Enter 0 for single filers, 1 for married filing jointly, 2 for married filing separately, and 3 for head of household. 
+
+Here are sample runs:
+
+```
+Enter the filing status: 0
+Enter the taxable income: 100000
+Tax is 21720.0
+```
+
+```
+Enter the filing status: 1
+Enter the taxable income: 300339
+Tax is 76932.87
+```
+
+```
+Enter the filing status: 2
+Enter the taxable income: 123500
+Tax is 29665.5
+```
+
+```
+Enter the filing status: 3
+Enter the taxable income: 4545402
+Tax is 1565250.7
+```
+
+PROBLEM STATEMENT: 
+Write a program that computes the personal income tax. 
+The program must prompt the user to enter filing status and taxable income along with tax computation. 
+Single Filers = 0
+Married: File Jointly = 1 
+Married: File Separately = 2 
+Head of Household = 3 
+
---
+++ src/ch3_project/ComputingTax.java
@@ -0,0 +1,133 @@
+
+/*
+ CIS 209: INTRO TO JAVA 
+ DUE: 10-4-17 
+ TARESSA HALPIN 
+ 
+ CHAPTER 3 PROJECT
+ COMPUTING TAX: 
+ FILING CASE SWITCH*/
+package ch3_project;
+
+import java.util.Scanner;
+
+public class ComputingTax {
+
+	  public static void main(String[] args)
+	  {
+		//SCANNER INPUT   
+		Scanner input = new Scanner(System.in); 
+		//PROMPTS USER TO ENTER 0-3 FOR SPECIFIC TAX FILINGS 
+		System.out.println("(Single Filer: 0, Married Jointly: 1 or " + "Qualified Widow_er, Married Separately: 2, "
+				+ "Head of House Hold: 3) Please Enter Filing Status: ");
+		//DECALRES NEW INTEGER 
+		int status = input.nextInt(); 
+		//PROMPTS USER TO ENTER TAX INCOME 
+		System.out.println("Enter the taxable income: ");
+		//DECLARES NEW DOUBLE DATA TYPE FOR INCOME
+		double income = input.nextDouble(); 
+		//TAX: 0 FOR DOUBLE DATA TYPE
+		double tax = 0; 
+		//SWITCH STATEMENT TO COMPUTE PERSONAL INCOME TAX: 
+		
+		//single filers: 
+		
+		if (status == 0)
+		{
+			if (income <= 8350)
+				tax = income * 0.10; 
+			else if (income <= 33950)
+				tax = 8350 * .10 + (income - 8350) * .15; 
+			else if (income <= 82250)
+				tax = 8350 * .10 + (33950 - 8350) * .15 + 
+				(income - 33950) * .25; 
+			else if (income <= 171550)
+				tax = 8350 * .10 + (33950 - 8350) * .15 + 
+				(82250 - 33950) * .25 + (income -82250) * .28;
+			else if (income <= 372590)
+				tax = 8350 * .10 + (33950 - 8350) * .15 + 
+				(82250 - 33950) * .25 + (171550 - 82250) * .28 + 
+				(income - 171550) * .33; 
+			else 
+				tax = 8350 * .10 + (33950 - 8350) * .15 + 
+				(82250 - 33950) * .25 + (171550 - 82250) * .28 + 
+				(372950 - 171550) * .33 + (income - 372950) * .35; 
+			
+		}
+		//MARRIED FILING JOINTLY OR QUALIFIED WIDOW
+		if (status == 1)
+		{
+			if (income <= 16700)
+				tax = income * 0.10; 
+			else if (income <= 67900)
+				tax = 16700 * .10 + (income - 16700) * .15; 
+			else if (income <= 137051)
+				tax = 16700 * .10 + (67900 - 16700) * .15 + 
+				(income - 67900) * .25; 
+			else if (income <= 208851)
+				tax = 16700 * .10 + (67900 - 16700) * .15 + 
+				(137050 - 67900) * .25 + (income - 137050) * .28;
+			else if (income <= 372951)
+				tax = 16700 * .10 + (67900 - 16700) * .15 + 
+				(137050 - 67900) * .25 + (208851 - 137050) * .28 + 
+				(income - 208851) * .33; 
+			else   
+				tax = 16700 * .10 + (67900 - 16700) * .15 + 
+				(137050 - 67900) * .25 + (208851 - 137050) * .28 + 
+				(372951 - 208851) * .33 + (income - 372951) * .35; 
+						
+		}
+		//MARRIED FILING SEPARATELY
+		if (status == 2)
+		{
+			if (income <= 8350)
+				tax = income * 0.10; 
+			else if (income <= 33950)
+				tax = 8350 * .10 + (income - 8350) * .15; 
+			else if (income <= 68525)
+				tax = 8350 * .10 + (33950 - 8350) * .15 + 
+				(income - 33950) * .25; 
+			else if (income <= 104425)
+				tax = 8350 * .10 + (33950 - 8350) * .15 + 
+				(68525 - 33950) * .25 + (income - 68525) * .28;
+			else if (income <= 186475)
+				tax = 8350 * .10 + (33950 - 8350) * .15 + 
+				(68525 - 33950) * .25 + (104425 - 68525) * .28 + 
+				(income - 104425) * .33; 
+			else 
+				tax = 8350 * .10 + (33950 - 8350) * .15 + 
+				(68525 - 33950) * .25 + (104425 - 68525) * .28 + 
+				(186475 - 104425) * .33 + (income - 186476) * .35; 
+			
+			
+		}
+		//HEAD OF HOUSEHOLD 
+		if (status == 3)
+		{
+			if (income <= 11950)
+				tax = income * 0.10; 
+			else if (income <= 45500)
+				tax = 11950 * .10 + (income - 11950) * .15; 
+			else if (income <= 117450)
+				tax = 11950 * .10 + (45500 - 11950) * .15 + 
+				(income - 45500) * .25; 
+			else if (income <= 190200)
+				tax = 11950 * .10 + (45500 - 11950) * .15 + 
+				(117450 - 45500) * .25 + (income - 117450) * .28;
+			else if (income <= 372951)
+				tax = 11950 * .10 + (45500 - 11950) * .15 + 
+				(117450 - 45500) * .25 + (190200 - 117450) * .28 + 
+				(income - 190200) * .33; 
+			else 
+				tax = 11950 * .10 + (45500 - 11950) * .15 + 
+				(117450 - 45500) * .25 + (190200 - 117450) * .28 + 
+				(372951 - 190200) * .33 + (income - 372951) * .35; 
+						
+		}
+		
+		//PRINTS OUT RESULT 
+		System.out.println("Personal Income Tax: " +(int)(tax*100) / 100.0);
+		  
+	  } 
+	
+}
